820ccdcb2f238c4a8c342d4c64b2ae0194c7ff7c
samdaphbynet <lj8@hotmail.fr>
2023-08-21 13:33:33 +0200

    update code

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,38 +3,32 @@
     script that, using this https://jsonplaceholder.typicode.com/, for a given
     employee ID, returns information about his/her TODO list progress.
 """
-import requests
-import sys
-
-
-def get_employee_todo(employee_id):
-    # Fetch user data
-    usr_response = requests.get
-    (f'https://jsonplaceholder.typicode.com/users/{employee_id}')
-    usr_data = usr_response.json()
-
-    # Fetch user's todos
-    todo_response = requests.get
-    (f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}')
-    todo_data = todo_response.json()
-
-    # Calculate the number of completed tasks
-    com_task = [task['title'] for task in todo_data if task['completed']]
-    num_com_task = len(com_task)
-    total_num_task = len(todo_data)
-
-    print(f"Employee {usr_data['name']} is done with tasks\
-          ({num_com_task}/{total_num_task}):")
-
-    # Print titles of completed tasks
-    for task in com_task:
-        print(f"\t{task}")
 
 
 if __name__ == "__main__":
+    import requests
+    import sys
     if len(sys.argv) != 2:
-        print(f"Usage: python {sys.argv[0]} <employee_id>")
-        sys.exit(1)
+        exit()
 
-    employee_id = int(sys.argv[1])
-    get_employee_todo(employee_id)
+    todos = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId={}&completed=true"
+        .format(sys.argv[1]))
+    name = requests.get(
+        "https://jsonplaceholder.typicode.com/users?id={}".format(sys.argv[1]))
+    name = name.json()
+    name = name[0]["name"]
+    todo = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId={}"
+        .format(sys.argv[1]))
+    todo = todo.json()
+    todo = len(todo)
+    todos = todos.json()
+    todo_list = []
+
+    for task in todos:
+        todo_list.append("\t {}".format(task["title"]))
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, len(todos), todo))
+    for i in todo_list:
+        print(i)
